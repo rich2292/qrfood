9874eb51376f8b91b93a15e160d5ee983f3dc10d
rich2292 <rich2292@yahoo.com.tw>
2020-08-03 16:29:21 +0800

    3223132

--- app/src/main/java/com/example/qrfoodproject/FoodDairy/chartModel.java
+++ app/src/main/java/com/example/qrfoodproject/FoodDairy/chartModel.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class chartModel {
+    //kmk
     private PieChart chart;
     private float user,total,percent;
     DecimalFormat df;
